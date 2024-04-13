d33ba00cf480d3e2aa3d5c191ba772b06365224b
mateusdias <mateus@mateus.pro.br>
2024-04-13 20:04:29 -0300

    First commit

---
+++ src/index.spec.ts
@@ -0,0 +1,16 @@
+import supertest from "supertest";
+import { server } from "./index";
+
+describe("server", () => {
+  const request = supertest.agent(server);
+  afterAll((done) => {
+    server.close(done);
+  });
+  it("should get /", async () => {
+    const res = await request.get("/");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      data: "It works...",
+    });
+  });
+});
